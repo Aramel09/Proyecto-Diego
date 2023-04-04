2ac443785769f8d56a5473a18f905c8045803f98
Alejandro Melara <alejandro.melara96@gmail.com>
2023-04-04 13:46:08 -0400

    Show pictures in UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,21 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <h1 className="flex justify-center text-center text-5xl font-bold">
+        Games
+      </h1>
+      <main className="flex justify-evenly">
+        <div className=" bg-black">Prueba</div>
+        <img
+          src="src/javier-martinez-hUD0PUczwJQ-unsplash.jpg"
+          alt="xbox controller"
+        ></img>
+        <div className=" bg-red-600">Caja</div>
+        <p>Prueba</p>
+        <img src="src/kenny-eliason-209FvE_57H8-unsplash.jpg" alt=""></img>
+      </main>
+    </>
+  );
 }
 
 export default App;
